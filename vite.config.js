5655d5b1c47b4fa72a2517ef8d73b959a33ede2c
Itshari2005 <hariprashath220305@gmail.com>
2025-07-30 10:01:46 +0530

    Updated Files

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'RealtyEngage',
+  base: '/RealtyEngage/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
